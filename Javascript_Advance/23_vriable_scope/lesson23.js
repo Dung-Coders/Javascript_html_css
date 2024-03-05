003630e8066fc13e81105fcda787e4db03c37cba
Dung-Coders <vandunghp1985@gmail.com>
2024-03-06 00:39:27 +0700

    Day 5

---
+++ Javascript_Advance/23_vriable_scope/lesson23.js
@@ -0,0 +1,16 @@
+// variable scope = where a variable is recognized
+//                  and accessible (local & global)
+
+let x =3;
+
+function2();//change 2=1 to see results.
+
+function function1(){
+    let x = 1;
+    console.log(x);
+}
+
+function function2(){
+    let x =2;
+    console.log(x);
+}
